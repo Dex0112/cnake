c6db98dd144ab57b9d85c140558cda9dd0d61670
Dex0112 <dexcrazy06@gmail.com>
2024-06-26 01:33:42 -0400

    Drawing Window

--- src/main.c
+++ src/main.c
@@ -1,5 +1,43 @@
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_events.h>
+#include <SDL2/SDL_image.h>
+#include <SDL2/SDL_render.h>
+#include <SDL2/SDL_video.h>
+#include <stdbool.h>
 #include <stdio.h>
 
+#define WINDOW_WIDTH 720
+#define WINDOW_HEIGHT 720
+
 int main() {
+    SDL_Init(SDL_INIT_VIDEO);
+
+    SDL_Window *window = SDL_CreateWindow("Tic-Tac-Toe", SDL_WINDOWPOS_CENTERED,
+                                          SDL_WINDOWPOS_CENTERED, WINDOW_WIDTH,
+                                          WINDOW_HEIGHT, SDL_WINDOW_SHOWN);
+
+    SDL_Renderer *renderer =
+        SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+
+    SDL_Event event;
+    bool running = true;
+
+    while (running) {
+        while (SDL_PollEvent(&event)) {
+            if (event.type == SDL_QUIT) {
+                running = false;
+            }
+        }
+
+        SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+        SDL_RenderClear(renderer);
+
+        SDL_RenderPresent(renderer);
+    }
+
+    SDL_DestroyWindow(window);
+    SDL_DestroyRenderer(renderer);
+    SDL_Quit();
+
     printf("Hello World\n");
 }
